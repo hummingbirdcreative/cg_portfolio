ac81af10a8e65702445726820efea96d178956c3
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-01-31 11:11:17 -1000

    home page configured with name h1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import image from "../sunnyclouds.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <img
+        src={image}
+        alt="sunny clouds"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="t4ext-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hi. I'm Christine
+        </h1>
+      </section>
+    </main>
   );
 };
 
