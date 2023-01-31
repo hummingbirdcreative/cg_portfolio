da9efa52e606fe2ab2b96767a15850ccfaf575c6
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-01-31 09:54:38 -1000

    app with sanity initialized

---
+++ cgblog/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cmc4cnq4',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+    projectId: "cmc4cnq4",
+    dataset: "production"
+})
