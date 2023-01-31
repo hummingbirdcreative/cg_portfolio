3d959268068158eb4e249da1f2f8ad7beb09a73e
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-01-31 10:49:21 -1000

    navbar links added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1>Home!</h1></div>
+    <div><h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from 'react-social-icons';
 
 const NavBar = () => {
   return (
-    <header>
-        <div>
-            <nav>
-                <NavLink to="/" exact>
+    <header className="bg-red-600">
+        <div className="container mx-auto flex justify-between">
+            <nav className="flex">
+                <NavLink className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest" activeClassName="text-white" to="/" exact>
                     CG
                 </NavLink>
-                <NavLink to="/post">
+                <NavLink className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" activeClassName="text-red-100 bg-red-700" to="/post">
                     Blog
                 </NavLink>
-                <NavLink to="/project">
+                <NavLink className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" activeClassName="text-red-100 bg-red-700" to="/project">
                     Projects
                 </NavLink>
-                <NavLink to="/about">
+                <NavLink className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" activeClassName="text-red-100 bg-red-700" to="/about">
                     About
                 </NavLink>
             </nav>
+            <div className="inline-flex py-3 px-3 my-6">
+                <SocialIcon url="https://github.com/hummingbirdcreative" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+                <SocialIcon url="https://www.linkedin.com/in/christinegrayb/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+            </div>
         </div>
     </header>
   )
