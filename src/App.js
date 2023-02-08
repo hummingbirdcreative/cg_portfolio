15c51bc03ed8dbb2f259d42565a850ac08cf3029
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-07 17:05:41 -1000

    post and skills section initiated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Brands from "./components/Brands";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
+import Skills from "./components/Skills";
 import Project from "./components/Project";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -15,6 +16,8 @@ function App() {
       <Home />
       <Brands />
       <About />
+      <Post />
+      <Skills />
       <Footer />
     </Router>
   );
