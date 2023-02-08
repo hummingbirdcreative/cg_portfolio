266b3578ee70e18b61c5b2e7ab2339310c82732a
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-07 15:46:24 -1000

    postcss config and tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    color: {
+      primary: colors.indigo,
+      secondary: colors.yellow,
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      fontFamily: {
+        sans: ["Jost", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
