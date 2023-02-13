5d3c83c1d68a0575186b5cffc7733f013c79f194
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-13 12:24:15 -1000

    sanity client.js updated

--- src/client.js
+++ src/client.js
@@ -1,6 +1,9 @@
-import sanityClient from '@sanity/client';
+import sanityClient from "@sanity/client";
 
 export default sanityClient({
-    projectId: "cmc4cnq4",
-    dataset: "production"
-})
+  projectId: "cmc4cnq4",
+  dataset: "production",
+  apiVersion: "2023-02-13",
+  token: "sanity-auth-token",
+  useCdn: false,
+});
