9231b90c338d2c1b4d19ca1c30932d939e856411
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-13 09:40:36 -1000

    single post page styled

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -42,41 +42,46 @@ const SinglePost = () => {
   return (
     <>
       <NavBarBlog />
-      <main className="bg-gray-200 min-h-screen p-12">
-        <article className="container mx-auto bg-green-100 rounded-lg">
-          <header className="relative">
-            <div className="absolute h-full w-full flex items-center justify-center p-8">
-              <div className="bg-white bg-opacity-75 rounded p-12">
-                <h1 className="cursive text-3xl lg:text-6xl mb-4">
-                  {singlePost.title}
-                </h1>
-                <div className="flex justify-center text-gray-800">
-                  <img
-                    src={urlFor(singlePost.authorImage).url()}
-                    alt={singlePost.name}
-                    className="w-10 h-10 rounded-full"
-                  />
-                  <p className="cursive flex items-center pl-2 text-2xl">
-                    {singlePost.name}
-                  </p>
+
+      <main className="wrapper">
+        <section data-scroll-index="3" className="section bg-orange-50">
+          <div className="container">
+            <article className="mx-auto bg-white">
+              <header className="relative">
+                <div className="absolute h-full w-full flex items-center justify-center p-8">
+                  <div className="bg-white bg-opacity-75 p-12">
+                    <h1 className=" text-3xl lg:text-6xl mb-4">
+                      {singlePost.title}
+                    </h1>
+                    <div className="flex justify-center text-gray-800">
+                      <img
+                        src={urlFor(singlePost.authorImage).url()}
+                        alt={singlePost.name}
+                        className="w-10 h-10 rounded-full"
+                      />
+                      <p className="cursive flex items-center pl-2 text-2xl">
+                        {singlePost.name}
+                      </p>
+                    </div>
+                  </div>
                 </div>
+                <img
+                  src={singlePost.mainImage.asset.url}
+                  alt={singlePost.title}
+                  className="w-full object-cover rounded-t"
+                  style={{ height: "400px" }}
+                />
+              </header>
+              <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+                <BlockContent
+                  blocks={singlePost.body}
+                  projectId="cmc4cnq4"
+                  dataset="production"
+                />
               </div>
-            </div>
-            <img
-              src={singlePost.mainImage.asset.url}
-              alt={singlePost.title}
-              className="w-full object-cover rounded-t"
-              style={{ height: "400px" }}
-            />
-          </header>
-          <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
-            <BlockContent
-              blocks={singlePost.body}
-              projectId="cmc4cnq4"
-              dataset="production"
-            />
+            </article>
           </div>
-        </article>
+        </section>
       </main>
       <Contact />
     </>
