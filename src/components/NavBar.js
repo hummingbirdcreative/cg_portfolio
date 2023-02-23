2fa2aed11ad620107ba3ea571667fc40ca5ac55c
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-22 16:01:04 -1000

    navbar back to original

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import Hamburger from "./Hamburger";
 
-export default function Nav() {
-  const [hamburgerOpen, setHamburgerOpen] = useState(false);
-
-  const toggleHamburger = () => {
-    setHamburgerOpen(!hamburgerOpen);
-  };
-
+const NavBar = () => {
   return (
     <header className="main-header fixed left-0 right-0 z-[111]">
       <div className="container flex items-center justify-between one-page-nav relative py-5 lg:py-3">
@@ -21,9 +15,13 @@ export default function Nav() {
             />
           </a>
         </div>
-        <div className="hamburger" onClick={toggleHamburger}>
-          <Hamburger isOpen={hamburgerOpen} />
-        </div>
+        {/* <Hamburger /> */}
+        <button className="lg:hidden mobile_toggle w-[40px] flex flex-col">
+          <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
+          <span className="w-[25px] h-[2px] bg-slate-900 inline-block my-[5px]"></span>
+          <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
+        </button>
+
         <div className="navbar-collapse flex">
           <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
             <li>
@@ -64,45 +62,8 @@ export default function Nav() {
           </a>
         </div>
       </div>
-
-      <style jsx>{`
-        .navigation {
-          width: 100%;
-          height: 50px;
-        }
-        .navigation ul {
-          display: flex;
-          flex-wrap: wrap;
-          float: right;
-          margin: 10px;
-          padding: 10px;
-          overflow: hidden;
-        }
-        .navigation ul li {
-          list-style-type: none;
-          padding-right: 25px;
-        }
-        .hamburger {
-          display: none;
-          z-index: 6;
-        }
-        @media (max-width: 767px) {
-          .hamburger {
-            display: fixed;
-            padding-top: 10px;
-            margin-left: 10px;
-            z-index: 6;
-          }
-          .navigation ul {
-            display: ${hamburgerOpen ? "inline" : "none"};
-            background-color: white;
-            height: 100vh;
-            width: 50vw;
-            margin-top: 50px;
-            position: fixed;
-          }
-        }
-      `}</style>
     </header>
   );
-}
+};
+
+export default NavBar;
