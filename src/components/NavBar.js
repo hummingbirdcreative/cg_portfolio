ccde867624bd23c1c876486b3518fe314cd100da
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-22 14:27:30 -1000

    nav on click divs added to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,53 +18,55 @@ const NavBar = () => {
           </a>
         </div>
         {/* <Hamburger /> */}
-        <button className="lg:hidden mobile_toggle menu_open w-[40px] flex flex-col">
-          <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
-          <span className="w-[25px] h-[2px] bg-slate-900 inline-block my-[5px]"></span>
-          <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
-        </button>
-
-        <div
-          className="navbar-collapse flex"
-          onClick={() => setIsNavOpen((prev) => !prev)}
-        >
-          <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
-            <li>
-              <a className="active" data-scroll-nav="0" href="#home">
-                <span>Home</span>
-              </a>
-            </li>
-            <li>
-              <a data-scroll-nav="1" href="#blog">
-                <span>Blog</span>
-              </a>
-            </li>
-            <li>
-              <a data-scroll-nav="2" href="#skill">
-                <span>Skills</span>
-              </a>
-            </li>
-            <li>
-              <a data-scroll-nav="3" href="#work">
-                <span>Portfolio</span>
-              </a>
-            </li>
-            <li>
-              <a data-scroll-nav="4" href="#contactus">
-                <span>Contact</span>
-              </a>
-            </li>
-          </ul>
+        <div onClick={() => setIsNavOpen((prev) => !prev)}>
+          <button className="lg:hidden mobile_toggle menu_open w-[40px] flex flex-col">
+            <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
+            <span className="w-[25px] h-[2px] bg-slate-900 inline-block my-[5px]"></span>
+            <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
+          </button>
         </div>
+        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+          <div onClick={() => setIsNavOpen(false)}>
+            <div className="navbar-collapse flex">
+              <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
+                <li>
+                  <a className="active" data-scroll-nav="0" href="#home">
+                    <span>Home</span>
+                  </a>
+                </li>
+                <li>
+                  <a data-scroll-nav="1" href="#blog">
+                    <span>Blog</span>
+                  </a>
+                </li>
+                <li>
+                  <a data-scroll-nav="2" href="#skill">
+                    <span>Skills</span>
+                  </a>
+                </li>
+                <li>
+                  <a data-scroll-nav="3" href="#work">
+                    <span>Portfolio</span>
+                  </a>
+                </li>
+                <li>
+                  <a data-scroll-nav="4" href="#contactus">
+                    <span>Contact</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-        <div className="ms-auto hidden lg:flex">
-          <a
-            className="px-btn px-btn-theme"
-            data-scroll-nav="4"
-            href="#contactus"
-          >
-            Contact Now
-          </a>
+            <div className="ms-auto hidden lg:flex">
+              <a
+                className="px-btn px-btn-theme"
+                data-scroll-nav="4"
+                href="#contactus"
+              >
+                Contact Now
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </header>
