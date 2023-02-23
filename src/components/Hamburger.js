ab06496eead1928e3e294e1fd56273c8865d29ee
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-02-22 14:56:09 -1000

    removed hamburger

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,79 +1,40 @@
-import { useState } from "react";
-
-export default function Hamburger() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
+export default function Hamburger({ isOpen }) {
   return (
-    <div className="flex items-center justify-between border-b border-gray-400 py-8">
-      <nav>
-        <section className="MOBILE-MENU lg:hidden mobile_toggle menu_open w-[40px] flex flex-col">
-          <div
-            className="HAMBURGER-ICON space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-            <span className="inline-block h-0.5 w-8 animate-pulse bg-slate-900"></span>
-            <span className="inline-block h-0.5 w-8 animate-pulse bg-slate-900"></span>
-            <span className="inline-block h-0.5 w-8 animate-pulse bg-slate-900"></span>
-          </div>
+    <>
+      <div className="hamburger">
+        <div className="burger burger1" />
+        <div className="burger burger2" />
+        <div className="burger burger3" />
+      </div>
 
-          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-            <div
-              className="absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                className="h-8 w-8 text-slate-900"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className="flex flex-col items-center justify-between min-h-[250px]">
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="#home">Home</a>
-              </li>
-              <li className="border-b border-slate-900 my-8 uppercase">
-                <a href="#blog">Blog</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="#skill">Skills</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="#work">Portfolio</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="#contactus">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </nav>
-
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </div>
+      <style jsx>{`
+        .hamburger {
+          width: 2rem;
+          height: 2rem;
+          display: flex;
+          justify-content: space-around;
+          flex-flow: column nowrap;
+          z-index: 10;
+        }
+        .burger {
+          width: 2rem;
+          height: 0.25rem;
+          border-radius: 10px;
+          background-color: black;
+          transform-origin: 1px;
+          transition: all 0.3s linear;
+        }
+        .burger1 {
+          transform: ${isOpen ? "rotate(45deg)" : "rotate(0)"};
+        }
+        .burger2 {
+          transform: ${isOpen ? "translateX(100%)" : "translateX(0)"};
+          opacity: ${isOpen ? 0 : 1};
+        }
+        .burger3 {
+          transform: ${isOpen ? "rotate(-45deg)" : "rotate(0)"};
+        }
+      `}</style>
+    </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Hamburger from "./Hamburger";
 
-const NavBar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+export default function Nav() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
   return (
     <header className="main-header fixed left-0 right-0 z-[111]">
@@ -17,60 +21,88 @@ const NavBar = () => {
             />
           </a>
         </div>
-        {/* <Hamburger /> */}
-        <div onClick={() => setIsNavOpen((prev) => !prev)}>
-          <button className="lg:hidden mobile_toggle menu_open w-[40px] flex flex-col">
-            <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
-            <span className="w-[25px] h-[2px] bg-slate-900 inline-block my-[5px]"></span>
-            <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
-          </button>
+        <div className="hamburger" onClick={toggleHamburger}>
+          <Hamburger isOpen={hamburgerOpen} />
         </div>
-        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-          <div onClick={() => setIsNavOpen(false)}>
-            <div className="navbar-collapse flex">
-              <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
-                <li>
-                  <a className="active" data-scroll-nav="0" href="#home">
-                    <span>Home</span>
-                  </a>
-                </li>
-                <li>
-                  <a data-scroll-nav="1" href="#blog">
-                    <span>Blog</span>
-                  </a>
-                </li>
-                <li>
-                  <a data-scroll-nav="2" href="#skill">
-                    <span>Skills</span>
-                  </a>
-                </li>
-                <li>
-                  <a data-scroll-nav="3" href="#work">
-                    <span>Portfolio</span>
-                  </a>
-                </li>
-                <li>
-                  <a data-scroll-nav="4" href="#contactus">
-                    <span>Contact</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="ms-auto hidden lg:flex">
-              <a
-                className="px-btn px-btn-theme"
-                data-scroll-nav="4"
-                href="#contactus"
-              >
-                Contact Now
+        <div className="navbar-collapse flex">
+          <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
+            <li>
+              <a className="active" data-scroll-nav="0" href="#home">
+                <span>Home</span>
               </a>
-            </div>
-          </div>
+            </li>
+            <li>
+              <a data-scroll-nav="1" href="#blog">
+                <span>Blog</span>
+              </a>
+            </li>
+            <li>
+              <a data-scroll-nav="2" href="#skill">
+                <span>Skills</span>
+              </a>
+            </li>
+            <li>
+              <a data-scroll-nav="3" href="#work">
+                <span>Portfolio</span>
+              </a>
+            </li>
+            <li>
+              <a data-scroll-nav="4" href="#contactus">
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ms-auto hidden lg:flex">
+          <a
+            className="px-btn px-btn-theme"
+            data-scroll-nav="4"
+            href="#contactus"
+          >
+            Contact Now
+          </a>
         </div>
       </div>
+
+      <style jsx>{`
+        .navigation {
+          width: 100%;
+          height: 50px;
+        }
+        .navigation ul {
+          display: flex;
+          flex-wrap: wrap;
+          float: right;
+          margin: 10px;
+          padding: 10px;
+          overflow: hidden;
+        }
+        .navigation ul li {
+          list-style-type: none;
+          padding-right: 25px;
+        }
+        .hamburger {
+          display: none;
+          z-index: 6;
+        }
+        @media (max-width: 767px) {
+          .hamburger {
+            display: fixed;
+            padding-top: 10px;
+            margin-left: 10px;
+            z-index: 6;
+          }
+          .navigation ul {
+            display: ${hamburgerOpen ? "inline" : "none"};
+            background-color: white;
+            height: 100vh;
+            width: 50vw;
+            margin-top: 50px;
+            position: fixed;
+          }
+        }
+      `}</style>
     </header>
   );
-};
-
-export default NavBar;
+}
